b176c0a8b4bac47143bd3f2b26a370247cdd57da
denissun <denis.sun@yahoo.com>
2019-03-13 15:42:33 -0400

    10 Layouts and Structure

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,8 @@
+   @extends('layouts.layout')
+
+   @section('content')
+
+   <h1> A place to show blog post </h1>
+
+
+   @endsection 
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,10 @@
 <?php
 
 Route::get('/', 'PostsController@index');
+Route::get('/posts/{post}', 'PostsController@show');
 
+// controller => PostsController
+
+// Eloquent model => Post
+
+// migration => create_posts_table
